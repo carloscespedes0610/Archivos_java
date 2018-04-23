ae04e48bf71e5ce27b4e5315bc1fa0609cf393f7
carloscespedes <cespedescarlos0610@gmail.com>
2018-04-22 23:33:56 -0400

    escritura y lectura binaria de varios objectos usando buffer revisar la cantida max. de caracteres en el nombre de la persona y revisar el leercon buffer de la clase Archivos Binarios solo lee 1000byte y la clase persona ya no debe implementacion Serializable

--- src/Archivos_Binarios/Persona.java
+++ src/Archivos_Binarios/Persona.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
  *
  * @author cespe
  */
-public class Persona implements Serializable{
+public class Persona{
     private int ci;
     private String nombre;
     private int edad;
